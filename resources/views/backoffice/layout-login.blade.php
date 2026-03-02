236554c1336e6f933dc81f7682ca6eb457fe1e65
Andrea Porcarelli <poandrea86@gmail.com>
2026-03-02 11:59:12 +0100

    Erp Api

--- resources/views/backoffice/layout-login.blade.php
+++ resources/views/backoffice/layout-login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     @include('backoffice.components.header')
-    <body data-mode="Light mode Default Desktop (L)">
+    <body data-mode="light mode desktop (L)">
         @yield('main-content')
         @include('backoffice.components.footer')
         @vite(['resources/css/app.scss', 'resources/js/app.js'])
