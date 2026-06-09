9c23a4f211540144998f9b336271f90a68e32fda
Andrea Porcarelli <poandrea86@gmail.com>
2026-06-09 17:55:54 +0200

    Fix

--- resources/views/whitelabel/layout.blade.php
+++ resources/views/whitelabel/layout.blade.php
@@ -13,8 +13,6 @@
     <link href="{{ asset('backoffice/css/helpers.css') }}" rel="stylesheet">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome per le icone -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/0708d92bf1.js" crossorigin="anonymous"></script>
