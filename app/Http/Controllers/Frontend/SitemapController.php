4e64a80b75695608f730c7327d82496ba70defae
Andrea Porcarelli <poandrea86@gmail.com>
2026-06-26 15:30:05 +0200

    Fix

--- app/Http/Controllers/Frontend/SitemapController.php
+++ app/Http/Controllers/Frontend/SitemapController.php
@@ -89,7 +89,7 @@ class SitemapController extends Controller
 
         $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
         $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" '
-              . 'xmlns:image="http://www.google.com/schemas/sitemaps-image/1.1">' . "\n";
+              . 'xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">' . "\n";
 
         foreach ($products as $product) {
             $loc = $product->route;
